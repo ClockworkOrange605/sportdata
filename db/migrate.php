15366166635264bd9faf40c94654e9e6682ee3ce
ak <kochkin.andrey@gmail.com>
2020-11-12 18:40:20 +0300

    db structure update

--- db/migrate.php
+++ db/migrate.php
@@ -4,48 +4,48 @@
     use Illuminate\Database\Capsule\Manager as Capsule;
 
     Capsule::schema()->create('sports', function ($table) {
-        $table->increments('id');
-        $table->integer('external_id')->unique();
+        $table->id();
+        $table->unsignedBigInteger('external_id')->unique();
         $table->string('name');
     });
 
     Capsule::schema()->create('countries', function ($table) {
-        $table->increments('id');
-        $table->integer('external_id')->unique();
+        $table->id();
+        $table->unsignedBigInteger('external_id')->unique();
         $table->string('name');
     });
 
     Capsule::schema()->create('leagues', function ($table) {
-        $table->increments('id');
-        $table->integer('external_id')->unique();
+        $table->id();
+        $table->unsignedBigInteger('external_id')->unique();
         $table->string('name');
     });
 
     Capsule::schema()->create('events', function ($table) {
-        $table->increments('id');
-        $table->integer('external_id')->unique();
-        $table->sports('sport_id')->references('id')->on('sports');
-        $table->foreign('country_id')->references('id')->on('countries');        
-        $table->integer('league_id')->references('id')->on('leagues');
+        $table->id();
+        $table->unsignedBigInteger('external_id')->unique();
+        $table->foreignId('sport_id')->constrained('sports');
+        $table->foreignId('country_id')->constrained('countries');
+        $table->foreignId('league_id')->constrained('leagues');
         $table->string('name');
         $table->string('team1');
         $table->string('team2');
-        $table->string('timestamp');
+        $table->timestamp('date');
     });
 
     Capsule::schema()->create('odd_types', function ($table) {
-        $table->increments('id');
-        $table->integer('external_id')->unique();
+        $table->id();
+        $table->unsignedBigInteger('external_id')->unique();
         $table->string('name');
     });
 
     Capsule::schema()->create('odds', function ($table) {
-        $table->increments('id');
-        $table->integer('external_id')->unique();
-        $table->foreign('event_id')->refernces('id')->on('events');
-        $table->integer('type_id')->references('id')->on('odd_types');
+        $table->id();
+        $table->unsignedBigInteger('external_id')->unique();
+        $table->foreignId('event_id')->constrained('events');
+        $table->foreignId('type_id')->constrained('odd_types');
         $table->string('name');
         $table->float('value');
     });
 
-    
+?>
--- db/rollback.php
+++ db/rollback.php
@@ -2,12 +2,13 @@
     require __DIR__ . '/../src/bootstrap.php';
 
     use Illuminate\Database\Capsule\Manager as Capsule;
-
+    
+    Capsule::schema()->dropIfExists('odds');
+    Capsule::schema()->dropIfExists('odd_types');
+    Capsule::schema()->dropIfExists('events');
     Capsule::schema()->dropIfExists('sports');
     Capsule::schema()->dropIfExists('countries');
     Capsule::schema()->dropIfExists('leagues');
-    Capsule::schema()->dropIfExists('events');
-    Capsule::schema()->dropIfExists('odd_types');
-    Capsule::schema()->dropIfExists('odds');
+    
 
 ?>
