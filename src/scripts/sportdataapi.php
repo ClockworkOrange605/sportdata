3d78db9ba3181bdb523e109fdf68cb05e554ffdf
ak <kochkin.andrey@gmail.com>
2020-11-19 22:10:14 +0300

    cleanup

--- src/scripts/sportdataapi.php
+++ src/scripts/sportdataapi.php
@@ -1,36 +1,45 @@
 <?php 
-    require __DIR__ . '/../vendor/autoload.php';
+    require __DIR__ . '/bootstrap.php';
 
     use Illuminate\Http\Client\PendingRequest;
 
-    $host = 'https://app.sportdataapi.com/api/v1/soccer';
-    $key = '186e7690-21d6-11eb-bc95-ed96806073c4';
+    $host = env('SPORDATAAPI_HOST');
+    $key = env('SPORDATAAPI_KEY');
 
-    // $counryID = 42; // England
-    // $leagueID = 237; // Premier League
-    // $seasonID = 352; // "20/21"
+    // $counryID = 1; // Africa
+    // $leagueID = 1; // CAF Super Cup
+    // $seasonID = 1; 
+
+    // $counryID = 7; // World
+    // $leagueID = 788; // World Cup
+    // $seasonID = 1193; // "2018"
+
+    $counryID = 42; // England
+    $leagueID = 237; // Premier League
+    $seasonID = 352; // "20/21"
 
     // $counryID = 102; // Russia
     // $leagueID = 504; // Premier League
     // $seasonID = 1477; // "20/21"
 
-    $counryID = 46; // France
-    $leagueID = 310; // Ligue 1
-    $seasonID = 455; // "20/21"
+    // $counryID = 46; // France
+    // $leagueID = 310; // Ligue 1
+    // $seasonID = 455; // "20/21"
 
     // $counryID = 48; // Germany
     // $leagueID = 314; // Bundesliga
     // $seasonID = 496; // "20/21"
+
+    $date_from = '2000-01-01';
+    $date_to = '2022-01-01';
 
     $matches = (new PendingRequest())->withHeaders([
         'apikey' => $key
     ])->get("{$host}/matches", [
         'season_id' => $seasonID,
         //'status_id' => 3, !!! Not working
-        // 'date_from' => '2020-11-01',
-        // 'date_to' => '2020-11-09'
-        'date_from' => '2020-11-16',
-        'date_to' => '2020-11-23'
+        'date_from' => $date_from,
+        'date_to' => $date_to
     ])->json('data');
 
     foreach($matches as $match){
@@ -66,7 +75,7 @@
             ) > 3
         ) {
 
-            //dd(data_get($odds, '1X2, Full Time Result'));
+            dd(data_get($odds, '1X2, Full Time Result.bookmakers.*.bookmaker_name'));
             dump(collect(data_get($odds, '1X2, Full Time Result.bookmakers.*.odds_data.home'))->avg());
             dump(collect(data_get($odds, '1X2, Full Time Result.bookmakers.*.odds_data.home'))->max());
             dump(collect(data_get($odds, '1X2, Full Time Result.bookmakers.*.odds_data.home'))->min());
